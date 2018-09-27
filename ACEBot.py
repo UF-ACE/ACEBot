569e1bea41ec34cc57046c36ff59467c7a141ee7
Alex Hartley <a.hartley@ufl.edu>
2018-09-27 12:06:15 -0400

    Linting

--- ACEBot.py
+++ ACEBot.py
@@ -2,81 +2,101 @@ import os
 from slackclient import SlackClient
 import re
 
+
 class acebot:
     def __init__(self):
         slack_token = os.environ["SLACK_API_TOKEN"]
-        self.sc=SlackClient(slack_token)
-        self.channelList=self.apiCall("conversations.list",exclude_archived=1)
+        self.sc = SlackClient(slack_token)
+        self.channelList = self.apiCall("conversations.list",
+                                        exclude_archived=1)
         self.replies = {}
         self.history = {}
         self.channelIDs = {}
         self.aceTunesURI = set()
+
     def apiCall(self, method, **kwargs):
-        response = self.sc.api_call(method,**kwargs)
+        response = self.sc.api_call(method, **kwargs)
         if not response['ok']:
             print(response['error'])
         if 'has_more' in response and response['has_more']:
             print("Has More")
         return response
+
     def updateChannelList(self):
-        self.channelList=self.apiCall("conversations.list",exclude_archived=1)
+        self.channelList = self.apiCall("conversations.list",
+                                        exclude_archived=1)
         for channel in self.channelList['channels']:
-            self.channelIDs[channel['name']]=channel['id']
+            self.channelIDs[channel['name']] = channel['id']
         return None
-    def getChannelID(self,name):
+
+    def getChannelID(self, name):
         if name not in self.channelIDs:
             self.updateChannelList()
         if name in self.channelIDs:
             return self.channelIDs[name]
         else:
             return None
-    def refreshConversationHistory(self,CID):
-        self.history[CID] =  self.apiCall("conversations.history",channel=CID)
-    def getConversationHistory(self,CID):
+
+    def refreshConversationHistory(self, CID):
+        self.history[CID] = self.apiCall("conversations.history",
+                                         channel=CID)
+
+    def getConversationHistory(self, CID):
         if CID not in self.history:
             self.refreshConversationHistory(CID)
         return self.history[CID]
-    def hasReply(self,message):
+
+    def hasReply(self, message):
         return 'reply_count' in message
-    def refreshReply(self,message,CID):
+
+    def refreshReply(self, message, CID):
         ts = message['ts']
         if CID not in self.replies:
             self.replies[CID] = {}
-        self.replies[CID][ts] = self.apiCall("conversations.replies",channel=CID,ts=ts)
-    def getReply(self,message,CID):
+        self.replies[CID][ts] = self.apiCall("conversations.replies",
+                                             channel=CID, ts=ts)
+
+    def getReply(self, message, CID):
         ts = message['ts']
         if CID not in self.replies:
-            self.refreshReply(message,CID)
+            self.refreshReply(message, CID)
         elif ts not in self.replies[CID]:
-            self.refreshReply(message,CID)
+            self.refreshReply(message, CID)
         return self.replies[CID][ts]
-    def getReplies(self,message,CID):
-        for reply in self.getReply(message,CID):
+
+    def getReplies(self, message, CID):
+        for reply in self.getReply(message, CID):
             yield reply
-    def getURI(self,url):
+
+    def getURI(self, url):
         if 'spotify' in url:
-            return re.search('(?<=track\/)(.+?)(?=\?)',url).group()
+            return re.search('(?<=track\/)(.+?)(?=\?)', url).group()
         print("Not Matched: " + url)
         return None
-    def getAttachmentLinks(self,message):
+
+    def getAttachmentLinks(self, message):
         if 'attachments' in message:
             for attachment in message['attachments']:
                 yield attachment['original_url']
-    def gatherReplyHistory(self,history,CID):
+
+    def gatherReplyHistory(self, history, CID):
         for message in history[CID]['messages']:
             if self.hasReply(message):
-                self.getReply(message,CID)
-    def iterateFullHistory(self,CID):
+                self.getReply(message, CID)
+
+    def iterateFullHistory(self, CID):
         self.getConversationHistory(CID)
         for message in self.history[CID]['messages']:
             if self.hasReply(message):
-                yield self.getReplies(message,CID)
+                yield self.getReplies(message, CID)
             else:
                 yield message
-    def iterateAttachmentLinks(self,CID):
+
+    def iterateAttachmentLinks(self, CID):
         for message in self.iterateFullHistory(CID):
             for url in self.getAttachmentLinks(message):
                 yield url
+
     def scrapeACETunes(self):
         CID = self.getChannelID('ace-tunes')
         for url in self.iterateAttachmentLinks(CID):
@@ -84,7 +104,8 @@ class acebot:
             if uri:
                 self.aceTunesURI.add(uri)
         return self.aceTunesURI
-    def getEmojiRanking(self,channelName):
+
+    def getEmojiRanking(self, channelName):
         CID = self.getChannelID(channelName)
         emoji = {}
         for message in self.iterateFullHistory(CID):
@@ -94,8 +115,9 @@ class acebot:
                         emoji[react['name']] = 0
                     emoji[react['name']] += react['count']
         emoji = list(emoji.items())
-        return(sorted(emoji,key=lambda x: x[1]))
+        return(sorted(emoji, key=lambda x: x[1]))
+
 
 bot = acebot()
-#print(bot.scrapeACETunes())
+# print(bot.scrapeACETunes())
 print(bot.getEmojiRanking('general'))
