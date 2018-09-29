86b73baf8c7784b5634086254ea920d26deda618
Daniela Isabel Travieso <dtravie@gmail.com>
2018-09-29 16:28:52 -0400

    function to add to playlist is working

--- ACEBot.py
+++ ACEBot.py
@@ -1,6 +1,11 @@
 import os
 from slackclient import SlackClient
 import re
+import pprint
+import sys
+
+import spotipy
+import spotipy.util as util
 
 
 class acebot:
@@ -13,7 +18,6 @@ class acebot:
         self.history = {}
         self.channelIDs = {}
         self.aceTunesURI = set()
-
     def apiCall(self, method, **kwargs):
         response = self.sc.api_call(method, **kwargs)
         if not response['ok']:
@@ -21,14 +25,12 @@ class acebot:
         if 'has_more' in response and response['has_more']:
             print("Has More")
         return response
-
     def updateChannelList(self):
         self.channelList = self.apiCall("conversations.list",
                                         exclude_archived=1)
         for channel in self.channelList['channels']:
             self.channelIDs[channel['name']] = channel['id']
         return None
-
     def getChannelID(self, name):
         if name not in self.channelIDs:
             self.updateChannelList()
@@ -36,26 +38,21 @@ class acebot:
             return self.channelIDs[name]
         else:
             return None
-
     def refreshConversationHistory(self, CID):
         self.history[CID] = self.apiCall("conversations.history",
                                          channel=CID)
-
     def getConversationHistory(self, CID):
         if CID not in self.history:
             self.refreshConversationHistory(CID)
         return self.history[CID]
-
     def hasReply(self, message):
         return 'reply_count' in message
-
     def refreshReply(self, message, CID):
         ts = message['ts']
         if CID not in self.replies:
             self.replies[CID] = {}
         self.replies[CID][ts] = self.apiCall("conversations.replies",
                                              channel=CID, ts=ts)
-
     def getReply(self, message, CID):
         ts = message['ts']
         if CID not in self.replies:
@@ -63,27 +60,22 @@ class acebot:
         elif ts not in self.replies[CID]:
             self.refreshReply(message, CID)
         return self.replies[CID][ts]
-
     def getReplies(self, message, CID):
         for reply in self.getReply(message, CID):
             yield reply
-
     def getURI(self, url):
         if 'spotify' in url:
             return re.search('(?<=track\/)(.+?)(?=\?)', url).group()
         print("Not Matched: " + url)
         return None
-
     def getAttachmentLinks(self, message):
         if 'attachments' in message:
             for attachment in message['attachments']:
                 yield attachment['original_url']
-
     def gatherReplyHistory(self, history, CID):
         for message in history[CID]['messages']:
             if self.hasReply(message):
                 self.getReply(message, CID)
-
     def iterateFullHistory(self, CID):
         self.getConversationHistory(CID)
         for message in self.history[CID]['messages']:
@@ -91,12 +83,10 @@ class acebot:
                 yield self.getReplies(message, CID)
             else:
                 yield message
-
     def iterateAttachmentLinks(self, CID):
         for message in self.iterateFullHistory(CID):
             for url in self.getAttachmentLinks(message):
                 yield url
-
     def scrapeACETunes(self):
         CID = self.getChannelID('ace-tunes')
         for url in self.iterateAttachmentLinks(CID):
@@ -104,7 +94,6 @@ class acebot:
             if uri:
                 self.aceTunesURI.add(uri)
         return self.aceTunesURI
-
     def getEmojiRanking(self, channelName):
         CID = self.getChannelID(channelName)
         emoji = {}
@@ -116,8 +105,18 @@ class acebot:
                     emoji[react['name']] += react['count']
         emoji = list(emoji.items())
         return(sorted(emoji, key=lambda x: x[1]))
-
-
-bot = acebot()
-# print(bot.scrapeACETunes())
-print(bot.getEmojiRanking('general'))
+    def addSongToPlaylist(self, username, playlist_id, track_id=list, scope='playlist-modify-public'):
+        # all inputs must be entered as a string
+        # username should be the id not the uri
+        token = util.prompt_for_user_token(username, scope)
+        if token:
+            sp = spotipy.Spotify(auth=token)
+            sp.trace = False
+            # user_playlist_add_tracks(self, user, playlist_id, tracks,
+            results = sp.user_playlist_add_tracks(user=username, playlist_id=playlist_id, tracks=track_id)
+            print results
+        else:
+            print "Can't get token for", username
+    # TODO: nice documentation for each function :^)
+    # TODO: function for checking duplicates
+    # TODO: spotify token refresh?
